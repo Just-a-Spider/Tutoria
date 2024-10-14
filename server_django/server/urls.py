792dc6a18a376e91c9875979fe9fd87c36bcf41e
Just-a-Spider <andre_arg_0116@outlook.com>
2024-10-14 07:27:42 -0500

    added "api" to the urls

--- server_django/server/urls.py
+++ server_django/server/urls.py
@@ -1,15 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include('user.api.urls', namespace='local')),
-    path('oauth/', include('social_django.urls', namespace='social')),
-    path('profiles/', include('profiles.api.urls')),
-    path('courses/', include('courses.api.urls')),
-    path('chats/', include('chat.api.urls')),
-    path('notifications/', include('notifications.api.urls')),
-    path('sessions/', include('tutoring_sessions.api.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('api/', include('server.api_urls')),
+]
