4bbd7c543ff3c2308ab15ed11755bf099d134789
Just-a-Spider <andre_arg_0116@outlook.com>
2024-12-01 18:43:23 -0500

    Switch database configuration to use SQLite for local development

--- server_django/server/settings.py
+++ server_django/server/settings.py
@@ -149,13 +149,11 @@ if DEBUG:
         }
     }
 else:
-    import dj_database_url
-
     DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get('HEROKU_POSTGRESQL_BLACK_URL'),
-            conn_max_age=600
-        )
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
 
 # Channels
