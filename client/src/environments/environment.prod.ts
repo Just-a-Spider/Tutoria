5d99c73299a2b9c45584ae04e54cc3e1a746ee2d
Just-a-Spider <andre_arg_0116@outlook.com>
2025-02-08 12:08:39 -0500

    Update README, Docker configurations, and environment settings for improved development workflow

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
-
 export const environment = {
   production: true,
-  hostUrl: 'http://localhost:8001/',
-  apiUrl: 'http://localhost:8001/api/',
+  hostUrl: 'http://localhost:8000/',
+  apiUrl: 'http://localhost:8000/api/',
   apiKey: '1eb2ed4b-db5b-4cc4-9846-79099f2735c2',
-  socketUrl: 'http://localhost:8001/ws/',
+  socketUrl: 'http://localhost:8000/ws/',
 };
